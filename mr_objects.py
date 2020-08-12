4fedcc7f671e04e3b02cda0b75ea5778f60e04c2
Paul Marsh <61422343+PTMGitHub@users.noreply.github.com>
2020-08-12 20:21:26 +0100

    How the group left it EOD 20200812

--- mr_objects.py
+++ mr_objects.py
@@ -19,11 +19,48 @@ class RoverPath:
 
     def get_rover_pos(self,recieved_command):
 
+        curr_rover_pos = [1,1,'N']
+
+        curr_x = curr_rover_pos[0]
+        curr_y = curr_rover_pos[1]
+        curr_d = curr_rover_pos[2]
+
+        direction = {0:"N",
+        1:"E",
+        2:"S",
+        3:"W"}
+        
+        # r = +1
+        # l = -1
+
+        # x = 0
+        # y = 0
+
+        # f =
+
+
         if recieved_command == ['f']:
-            return [1,2,'N']
+            # return [1,2,'N']
+            if curr_rover_pos[2] == 'N':
+                curr_y = curr_y +1
+            if curr_rover_pos[2] == 'E':
+                curr_x = curr_x +1          
+            if curr_rover_pos[2] == 'S':
+                curr_y = curr_y -1
+            if curr_rover_pos[2] == 'W':
+                curr_x = curr_x -1            
+
 
         if recieved_command == ['r']:
-            return [1,1,'E']
+            # return [1,1,'E']
+            if curr_rover_pos[2] == 'N':
+                curr_x = curr_x -1
+            if curr_rover_pos[2] == 'E':
+                curr_x = curr_y -1          
+            if curr_rover_pos[2] == 'S':
+                curr_x = curr_x +1
+            if curr_rover_pos[2] == 'W':
+                curr_x = curr_y +1     
 
         if recieved_command == ['l']:
             return [1,1,'W']
@@ -31,7 +68,7 @@ class RoverPath:
         if recieved_command == ['f','r']:
             return [1,2,'E']
         
-        curr_rover_pos = self.initial_rover_pos
+        curr_rover_pos = [curr_x,curr_y,curr_d]
 
-        return self.initial_rover_pos
+        return curr_rover_pos
 
--- mr_tests.py
+++ mr_tests.py
@@ -4,28 +4,28 @@ import mr_objects
 
 #The test cases
 class MarsRoverTest(unittest.TestCase):
-    #Test 1
-    def test_working(self):
-        self.assertTrue(True)
+    # #Test 1
+    # def test_working(self):
+    #     self.assertTrue(True)
 
-    def test_planet_exists(self):
-        self.assertTrue(True)
+    # def test_planet_exists(self):
+    #     self.assertTrue(True)
     
 
-    def test_know_planet_size(self):
-        expected_planet_size = [10,10]
-        #This line iniciates an instance of the planet class from mr_objects.py
-        planet = mr_objects.Planet()
-        # print("*** test_know_planet_size start ***")
-        # print(mr_objects.planet())
-        # print("*** test_know_planet_size end ***")
-        self.assertEqual(planet.get_planet_size(), expected_planet_size)
+    # def test_know_planet_size(self):
+    #     expected_planet_size = [10,10]
+    #     #This line iniciates an instance of the planet class from mr_objects.py
+    #     planet = mr_objects.Planet()
+    #     # print("*** test_know_planet_size start ***")
+    #     # print(mr_objects.planet())
+    #     # print("*** test_know_planet_size end ***")
+    #     self.assertEqual(planet.get_planet_size(), expected_planet_size)
 
-    def test_current_position(self):
-        expected_current_pos = [1,1,'N']
-        passed_command = ''
-        rover_pos = mr_objects.RoverPath()
-        self.assertEqual(rover_pos.get_rover_pos(passed_command),expected_current_pos)
+    # def test_current_position(self):
+    #     expected_current_pos = [1,1,'N']
+    #     passed_command = ''
+    #     rover_pos = mr_objects.RoverPath()
+    #     self.assertEqual(rover_pos.get_rover_pos(passed_command),expected_current_pos)
     
     def test_move_rover_forward_by_one(self):
         expected_current_pos = [1,2,'N']
@@ -33,20 +33,22 @@ class MarsRoverTest(unittest.TestCase):
         rover_pos = mr_objects.RoverPath()
         self.assertEqual(rover_pos.get_rover_pos(passed_command),expected_current_pos)
 
-    def test_rover_turn_right(self):
-        expected_current_pos = [1,1,'E']
-        passed_command = ['r']
-        rover_pos = mr_objects.RoverPath()
-        self.assertEqual(rover_pos.get_rover_pos(passed_command),expected_current_pos)
+    # def test_rover_turn_right(self):
+    #     expected_current_pos = [1,1,'E']
+    #     passed_command = ['r']
+    #     rover_pos = mr_objects.RoverPath()
+    #     self.assertEqual(rover_pos.get_rover_pos(passed_command),expected_current_pos)
 
-    def test_rover_turn_left(self):
-        expected_current_pos = [1,1,'W']
-        passed_command = ['l']
-        rover_pos = mr_objects.RoverPath()
-        self.assertEqual(rover_pos.get_rover_pos(passed_command),expected_current_pos)
+    # def test_rover_turn_left(self):
+    #     expected_current_pos = [1,1,'W']
+    #     passed_command = ['l']
+    #     rover_pos = mr_objects.RoverPath()
+    #     self.assertEqual(rover_pos.get_rover_pos(passed_command),expected_current_pos)
 
-    def test_rover_forward_turn_right(self):
-        expected_current_pos = [1,2,'E']
-        passed_command = ['f','r']
-        rover_pos = mr_objects.RoverPath()
-        self.assertEqual(rover_pos.get_rover_pos(passed_command),expected_current_pos)
+    # def test_rover_forward_turn_right(self):
+    #     expected_current_pos = [1,2,'E']
+    #     passed_command = ['f','r']
+    #     rover_pos = mr_objects.RoverPath()
+    #     self.assertEqual(rover_pos.get_rover_pos(passed_command),expected_current_pos)
+
+    
